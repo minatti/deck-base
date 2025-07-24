353a9bebc1ac38d2f9e8802da384391ecacfc65c
minatti <luancarminatt@gmail.com>
2025-07-23 22:01:11 -0300

    feito: criado a funcao caminho_baralho

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import os
+
+# Função para obter o caminho da pasta do baralho
+def caminho_baralho():
+    PASTA_BASE = os.path.abspath(os.path.dirname(__file__))
+    PASTA_BARALHO = os.path.join(PASTA_BASE, "data")
+
+    if not os.path.exists(PASTA_BARALHO):
+        os.makedirs(PASTA_BARALHO)
+
+    PASTA_BARALHO = os.path.join(PASTA_BARALHO, "deck.json")
+
+    return PASTA_BARALHO
+
 def main():
     print("Iniciando o backend do deck base...")
+    print("Caminho do baralho:", caminho_baralho())
 
 
 if __name__ == "__main__":
